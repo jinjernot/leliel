e980e7e5070231240b52d99d8d7e9ad005f75650
jinjernot <norindes@gmail.com>
2023-12-05 20:17:34 -0600

    def init

--- main.py
+++ main.py
@@ -4,50 +4,56 @@ import pandas as pd
 import requests
 import json
 
-url = "https://hermesws.ext.hp.com/HermesWS/secure/v2/productcontent"
- 
-try:
-    ca_cert = ca_cert_path
-    client_cert = (client_cert_path, client_key_path)
 
-    json_data = {
-    "sku": [
-      "8EP60AAE"
-    ],
-    "countryCode": "GB",
-    "languageCode": "EN",
-    "layoutName": "PDPCOMBO",
-    "requestor": "HERMESQA-PRO",
-    "reqContent": [
-      "chunks",
-      "images",
-      "hierarchy",
-      "plc"
-    ]
-    }
 
-    response = requests.post(
-        url,
-        cert=client_cert,
-        verify=ca_cert,
-        headers={'Content-Type': 'application/json'},
-        data=json.dumps(json_data)
-    )
+def api_call():
+  url = "https://hermesws.ext.hp.com/HermesWS/secure/v2/productcontent"
+  
+  try:
+      ca_cert = ca_cert_path
+      client_cert = (client_cert_path, client_key_path)
 
-    if response.status_code == 200:
-        print("Request successful!")
-        print(response.json())
+      json_data = {
+      "sku": [
+        "8EP60AAE"
+      ],
+      "countryCode": "GB",
+      "languageCode": "EN",
+      "layoutName": "PDPCOMBO",
+      "requestor": "HERMESQA-PRO",
+      "reqContent": [
+        "chunks",
+        "images",
+        "hierarchy",
+        "plc"
+      ]
+      }
 
-       # Convert the API response to a DataFrame
-        response_data = response.json()
-        df = pd.json_normalize(response_data)
+      response = requests.post(
+          url,
+          cert=client_cert,
+          verify=ca_cert,
+          headers={'Content-Type': 'application/json'},
+          data=json.dumps(json_data)
+      )
 
-        # Save the DataFrame to an Excel file
-        excel_file_path = "api_results.xlsx"
-        df.to_excel(excel_file_path, index=False)
+      if response.status_code == 200:
+          print("Request successful!")
+          print(response.json())
 
-    else:
-        print(f"Request failed with status code {response.status_code}")
-        print(f"Response content: {response.text}")
-except Exception as e:
-    print(f"An error occurred: {e}")
+        # Convert the API response to a DataFrame
+          response_data = response.json()
+          df = pd.json_normalize(response_data)
+
+          # Save the DataFrame to an Excel file
+          excel_file_path = "api_results.xlsx"
+          df.to_excel(excel_file_path, index=False)
+
+      else:
+          print(f"Request failed with status code {response.status_code}")
+          print(f"Response content: {response.text}")
+  except Exception as e:
+      print(f"An error occurred: {e}")
+
+
+api_call()
