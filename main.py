c34101e062d0b60e5c737a3ceccfe564e8d42abe
Jinjernot <norindes@gmail.com>
2023-12-08 21:14:20 -0600

    first template

--- main.py
+++ main.py
@@ -1,59 +1,59 @@
-from config import ca_cert_path, client_cert_path, client_key_path, client_pfx_path, passphrase
+from config import ca_cert_path, client_cert_path, client_key_path, url
 
+import json
 import pandas as pd
 import requests
-import json
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+app.use_static_for = 'static'
 
+@app.route('/')
+def index():
+    return render_template("index.html")
 
-def api_call():
-  url = "https://hermesws.ext.hp.com/HermesWS/secure/v2/productcontent"
-  
-  try:
-      ca_cert = ca_cert_path
-      client_cert = (client_cert_path, client_key_path)
+@app.route('/get_product', methods=['POST'])
+def get_product():
+    try:
+        ca_cert = ca_cert_path
+        client_cert = (client_cert_path, client_key_path)
 
-      json_data = {
-      "sku": [
-        "8EP60AAE"
-      ],
-      "countryCode": "GB",
-      "languageCode": "EN",
-      "layoutName": "PDPCOMBO",
-      "requestor": "HERMESQA-PRO",
-      "reqContent": [
-        "chunks",
-        "images",
-        "hierarchy",
-        "plc"
-      ]
-      }
+        sku = request.form.get('sku')
+        country_code = request.form.get('country')
 
-      response = requests.post(
-          url,
-          cert=client_cert,
-          verify=ca_cert,
-          headers={'Content-Type': 'application/json'},
-          data=json.dumps(json_data)
-      )
+        json_data = {
+            "sku": [sku],
+            "countryCode": country_code,
+            "languageCode": "EN",
+            "layoutName": "PDPCOMBO",
+            "requestor": "HERMESQA-PRO",
+            "reqContent": ["chunks", "images", "hierarchy", "plc"]
+        }
 
-      if response.status_code == 200:
-          print("Request successful!")
-          print(response.json())
+        response = requests.post(
+            url,
+            cert=client_cert,
+            verify=ca_cert,
+            headers={'Content-Type': 'application/json'},
+            data=json.dumps(json_data)
+        )
 
-        # Convert the API response to a DataFrame
-          response_data = response.json()
-          df = pd.json_normalize(response_data)
+        if response.status_code == 200:
+            print("Request successful!")
+            print(response.json())
 
-          # Save the DataFrame to an Excel file
-          excel_file_path = "api_results.xlsx"
-          df.to_excel(excel_file_path, index=False)
+            # Save the API response to a JSON file
+            with open("api_response.json", "w") as json_file:
+                json.dump(response.json(), json_file)
+            product_data = response.json()
 
-      else:
-          print(f"Request failed with status code {response.status_code}")
-          print(f"Response content: {response.text}")
-  except Exception as e:
-      print(f"An error occurred: {e}")
+            return render_template("product.html", product_data=product_data)
+        else:
+            print(f"Request failed with status code {response.status_code}")
+            print(f"Response content: {response.text}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return render_template("error.html")
 
-
-api_call()
+if __name__ == '__main__':
+    app.run(debug=True)
