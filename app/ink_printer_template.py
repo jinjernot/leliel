7a6b821941be033956a749e48e5a5a14a0e3942f
jinjernot <norindes@gmail.com>
2024-04-22 17:55:04 -0600

    ink template pre release version

--- app/ink_printer_template.py
+++ app/ink_printer_template.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import json
 
-def build_laptop(api_response):
+
+def build_template_ink(api_response):
     """
     Builds a template DataFrame from the API response containing product details.
 
@@ -20,7 +21,7 @@ def build_laptop(api_response):
             return {}
 
     # Load the tags from a JSON file
-    with open("/opt/ais/app/python/api/app/data/tags_laptop.json", "r") as f:
+    with open("/opt/ais/app/python/api/app/data/tags_ink_printer.json", "r") as f:
         tags_data = json.load(f)
 
     # ATF Content
