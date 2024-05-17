50c8bc48068708b3993192ffed3c48eb18ca1829
jinjernot <norindes@gmail.com>
2024-05-17 12:16:27 -0600

    mouseover update

--- static/js/mouseover.js
+++ static/js/mouseover.js
@@ -7,13 +7,10 @@ function handleMouseover(element, replacementText) {
   popup.style.border = '1px solid #ccc'; 
   popup.style.padding = '5px'; 
   popup.style.zIndex = '1000';
-  popup.style.width = (2 * parseFloat(getComputedStyle(popup).width)) + 'px';
-  popup.style.height = (2 * parseFloat(getComputedStyle(popup).height)) + 'px';
-  
   
   var rect = element.getBoundingClientRect();
   popup.style.top = (rect.top + window.pageYOffset) + 'px';
-  popup.style.left = (rect.left + window.pageXOffset) + 'px';
+  popup.style.left = (rect.right + window.pageXOffset) + 'px';
 
   document.body.appendChild(popup);
 
