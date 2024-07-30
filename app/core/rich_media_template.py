801e938845d21e5efdd2cdb7831296e1f79e2f42
jinjernot <norindes@gmail.com>
2024-07-30 14:45:07 -0600

    include more images, Rich Media

--- app/core/rich_media_template.py
+++ app/core/rich_media_template.py
@@ -4,34 +4,38 @@ import json
 def build_template_rich_media(response_json, sku):
     try:
         product = response_json.get('products', {}).get(sku, {})
-        images = product.get('richmedia', {}).get('Image - Annotated', [])
-        sku = product.get('sku', [])
+        
+        # Define the image types you want to include
+        image_types = ['Image - Annotated', 'Image - At a glance', 'Image - Banners']
         
         # Create a list to hold all image details
         all_image_details = []
         image_count = 0
 
-        # Iterate over each image and extract details
-        for image in images:
-            image_data = {
-                "assetCategory": image.get('assetCategory'),
-                "assetDescription": image.get('assetDescription'),
-                "assetId": image.get('assetId'),
-                "assetModifiedDate": image.get('assetModifiedDate'),
-                "assetName": image.get('assetName'),
-                "assetTitle": image.get('assetTitle'),
-                "background": image.get('background'),
-                "imageDetail": image.get('imageDetail'),
-                "imageUrlHttps": image.get('imageUrlHttps'),
-                "keyword": image.get('keyword'),
-                "languageCodes": image.get('languageCodes'),
-                "orientation": image.get('orientation'),
-                "pixelHeight": image.get('pixelHeight'),
-                "pixelWidth": image.get('pixelWidth'),
-                "renditionId": image.get('renditionId')
-            }
-            all_image_details.append(image_data)
-            image_count += 1
+        # Iterate over each image type and extract details
+        for image_type in image_types:
+            images = product.get('richmedia', {}).get(image_type, [])
+            
+            for image in images:
+                image_data = {
+                    "assetCategory": image.get('assetCategory'),
+                    "assetDescription": image.get('assetDescription'),
+                    "assetId": image.get('assetId'),
+                    "assetModifiedDate": image.get('assetModifiedDate'),
+                    "assetName": image.get('assetName'),
+                    "assetTitle": image.get('assetTitle'),
+                    "background": image.get('background'),
+                    "imageDetail": image.get('imageDetail'),
+                    "imageUrlHttps": image.get('imageUrlHttps'),
+                    "keyword": image.get('keyword'),
+                    "languageCodes": image.get('languageCodes'),
+                    "orientation": image.get('orientation'),
+                    "pixelHeight": image.get('pixelHeight'),
+                    "pixelWidth": image.get('pixelWidth'),
+                    "renditionId": image.get('renditionId')
+                }
+                all_image_details.append(image_data)
+                image_count += 1
 
         # Calculate counts for each attribute
         counts = {
@@ -41,6 +45,7 @@ def build_template_rich_media(response_json, sku):
         }
 
         return {"image_details": all_image_details, "image_count": image_count, "counts": counts, "sku": sku}
+    
     except KeyError as e:
         # Handle missing keys in the JSON response
         error_message = f"Key error: {str(e)}"
