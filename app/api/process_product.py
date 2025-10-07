addb03b483fed82616446051dcf870b0352ee7f9
jinjernot <norindes@gmail.com>
2025-10-07 12:03:01 -0600

    translations

--- app/api/process_product.py
+++ app/api/process_product.py
@@ -20,6 +20,7 @@ def process_api_response(response_json, sku, locales=None, country_code=None, la
     """
     try:
         current_locale = f"{country_code}-{language_code}" if country_code and language_code else None
+        translations = current_app.config['TRANSLATIONS'].get(language_code, current_app.config['TRANSLATIONS']['en'])
         product = response_json.get('products', {}).get(sku, {})
         product_type = get_product_type(product)
         companions = build_template_companions(response_json, sku)
@@ -97,7 +98,7 @@ def process_api_response(response_json, sku, locales=None, country_code=None, la
                     if len(feature_blocks) >= feature_config['MAX_BLOCKS']:
                         break
 
-        return render_template('product_template.html', df=df, tech_specs_by_group=sorted_tech_specs_by_group, df_images=df_images, companions=companions, top_companions=top_companions, df_footnotes=df_footnotes, df_disclaimers=df_disclaimers, mm_blocks=mm_blocks, feature_blocks=feature_blocks, top_components=top_components_list, video_data=video_data, locales=locales, sku=sku, current_locale=current_locale, country_names=current_app.config['COUNTRY_NAMES'], locale_names=current_app.config['LOCALE_NAMES'])
+        return render_template('product_template.html', df=df, tech_specs_by_group=sorted_tech_specs_by_group, df_images=df_images, companions=companions, top_companions=top_companions, df_footnotes=df_footnotes, df_disclaimers=df_disclaimers, mm_blocks=mm_blocks, feature_blocks=feature_blocks, top_components=top_components_list, video_data=video_data, locales=locales, sku=sku, current_locale=current_locale, country_names=current_app.config['COUNTRY_NAMES'], locale_names=current_app.config['LOCALE_NAMES'], translations=translations)
     except KeyError as e:
         logging.error(
             f"A KeyError occurred in process_api_response: {e}", exc_info=True)
